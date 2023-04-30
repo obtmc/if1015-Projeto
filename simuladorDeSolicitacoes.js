f2b21194852e44dcf0cfc5e5792ba3f14d374df5
obtmc <olivia7cmr@gmail.com>
2023-04-30 16:32:54 -0300

    simulador rodando e dataset correspondente

--- simuladorDeSolicitacoes.js
+++ simuladorDeSolicitacoes.js
@@ -1,54 +1,15 @@
 const amqp = require('amqplib/callback_api');
 const csv = require('csv-parser');
 const fs = require('fs');
-const amqp = require('amqplib');
 
-const csvFilePath = 'dados.csv'; // caminho para o arquivo CSV
-const rabbitMQUrl = 'amqp://localhost'; // URL do servidor RabbitMQ
+const csvFilePath = 'dataset_tratado.csv'; // caminho para o arquivo CSV
+const rabbitMQUrl = 'amqp://localhost'; // brockerRabbitMQ - URL do servidor RabbitMQ
 const exchangeName = 'dados'; // nome da exchange RabbitMQ
 const routingKey = 'dados'; // routing key para as mensagens
-
 const delay = 1000; // tempo de espera entre envio de mensagens (em milissegundos)
-const timestampIndex = 0; // índice da coluna com informações de data e hora
-
-// Conecta ao servidor RabbitMQ
-amqp.connect(rabbitMQUrl)
-  .then((conn) => {
-    return conn.createChannel();
-  })
-  .then((ch) => {
-    // Declara a exchange
-    ch.assertExchange(exchangeName, 'direct', { durable: false });
-    
-    // Lê o arquivo CSV linha por linha
-    fs.createReadStream(csvFilePath)
-      .pipe(csv())
-      .on('data', (row) => {
-        // Modifica a informação de data e hora para o momento de envio
-        row[timestampIndex] = new Date().toISOString();
-        
-        // Converte a linha em uma mensagem JSON
-        const message = JSON.stringify(row);
-        
-        // Envia a mensagem para o RabbitMQ
-        ch.publish(exchangeName, routingKey, Buffer.from(message));
-        
-        // Aguarda o tempo de delay//esse tempo deve ser multiplicado pelo valor de data e hora normalizado
-        setTimeout(() => {}, delay);
-      })
-      .on('end', () => {
-        console.log('Envio concluído');
-      });
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-
-
-
+let delayM =1;
 // Conexão com o RabbitMQ
-amqp.connect('amqp://localhost', function(error0, connection) {
+amqp.connect(rabbitMQUrl, function(error0, connection) {
   if (error0) {
     throw error0;
   }
@@ -59,19 +20,42 @@ amqp.connect('amqp://localhost', function(error0, connection) {
       throw error1;
     }
 
-    // Função que envia a solicitação para o broker
-    function enviarSolicitacao(solicitacao) {
-      channel.assertExchange('solicitacoes', 'fanout', {
-        durable: false
-      });
-      channel.publish('solicitacoes', '', Buffer.from(JSON.stringify(solicitacao)));
-      console.log("Solicitação enviada:", solicitacao);
+    // Função que envia a mensagem para o broker
+    function enviarMensagem(mensagem) {
+      // Converte a mensagem em uma string JSON
+      const message = JSON.stringify(mensagem);
+
+      // Aguarda o tempo de delay antes de enviar a próxima mensagem
+      setTimeout(() => {
+        console.log("Mensagem enviada:", message);
+        // Envia a mensagem para o RabbitMQ
+        channel.publish(exchangeName, routingKey, Buffer.from(message));
+      }, delay*delayM);
     }
 
-    // Cliente envia a solicitação para o broker
-    const solicitacao = { tipo: "solicitar_pedido", dados: { cliente: "João", produto: "Camisa", quantidade: 2 } };
-    enviarSolicitacao(solicitacao);
+    // Lê o arquivo CSV linha por linha
+    fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on('data', (row) => {
 
-    console.log("Aguardando resposta...");
+      // Modifica a informação de data e hora para o momento de envio
+      row['solicitacao_data_hora'] = new Date().toISOString();
+      delayM = delay*row['delay'];
+      delete row['delay'];
+
+      // Converte a linha em uma mensagem JSON
+      const solicitacao = JSON.stringify(row);
+
+      // Envia a mensagem para o broker
+      enviarMensagem(solicitacao);
+    })
+
+    .on('end', () => {
+      console.log('Envio concluído');
+      // Fecha a conexão com o RabbitMQ após o envio de todas as mensagens
+      setTimeout(() => {
+        connection.close();
+      }, delay*delayM);
+    });
   });
 });
